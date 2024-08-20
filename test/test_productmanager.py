e1754005adea6c9eb5e43144111443a53321fe53
Milad Zarei <miliaxe0@gmail.com>
2024-08-20 16:36:27 +0330

    chore: add more tests for ProductManager

--- test/test_productmanager.py
+++ test/test_productmanager.py
@@ -15,30 +15,29 @@ class TestProductManager(unittest.TestCase):
                 description="Product 1",
                 brand="BrandA",
                 price=1000,
-                count_in_carton=10
+                count_in_carton=10,
             ),
             ProductInfo(
                 code="P002",
                 description="Product 2",
                 brand="BrandB",
                 price=1234567890,
-                count_in_carton=20
+                count_in_carton=20,
             ),
             ProductInfo(
                 code="P003",
                 description="Product 3",
                 brand="BrandC",
                 price=10000,
-                count_in_carton=100
-            )
+                count_in_carton=100,
+            ),
         ]
-        
+
         self.sample_product_list = [
-            ["P001", "Product 1", "BrandA",  10, 1000],
-            ["P002", "Product 2", "BrandB",  20, 1234567890],
-            ["P003", "Product 3", "BrandC",  100, 10000]
-        ]    
-    
+            ["P001", "Product 1", "BrandA", 10, 1000],
+            ["P002", "Product 2", "BrandB", 20, 1234567890],
+            ["P003", "Product 3", "BrandC", 100, 10000],
+        ]
 
     @patch.object(Product, "get")
     def test_product_update(self, mock_get):
@@ -135,32 +134,144 @@ class TestProductManager(unittest.TestCase):
         mock_method.assert_called()
         mock_method.assert_any_call(self.sample_product_infos[0])
         mock_method.assert_any_call(self.sample_product_infos[1])
-    
 
-    @patch('rich.print')
-    @patch('rich.table.Table.add_row')
+    @patch("rich.print")
+    @patch("rich.table.Table.add_row")
     def test_print_products(self, mock_add_row: Mock, mock_print: Mock):
         ProductManager.print_products(self.sample_product_infos)
-        
+
         mock_print.assert_called_once()
-        mock_add_row.assert_any_call(self.sample_product_infos[0].code, self.sample_product_infos[0].description, self.sample_product_infos[0].brand, "10", "IRR 1,000")
-        mock_add_row.assert_any_call(self.sample_product_infos[1].code, self.sample_product_infos[1].description, self.sample_product_infos[1].brand, "20", "IRR 1,234,567,890")
-        
-    
+        mock_add_row.assert_any_call(
+            self.sample_product_infos[0].code,
+            self.sample_product_infos[0].description,
+            self.sample_product_infos[0].brand,
+            "10",
+            "IRR 1,000",
+        )
+        mock_add_row.assert_any_call(
+            self.sample_product_infos[1].code,
+            self.sample_product_infos[1].description,
+            self.sample_product_infos[1].brand,
+            "20",
+            "IRR 1,234,567,890",
+        )
+
     def test_save_products(self):
         with tempfile.NamedTemporaryFile() as tmpfile:
             filename = tmpfile.name + ".xlsx"
             ProductManager.save_products(filename, self.sample_product_infos)
-            
+
             reader = SheetReader(filename)
             data = reader.get_data()
-            
+
             self.assertEqual(data[0], ["1"] + self.sample_product_list[0])
             self.assertEqual(data[1], ["2"] + self.sample_product_list[1])
             self.assertEqual(data[2], ["3"] + self.sample_product_list[2])
-        
-        
-    
+
+    @patch.object(Product, "remove")
+    def test_product_remove(self, mock_remove: Mock):
+        ProductManager.remove("PA69420")
+        mock_remove.assert_called_once()
+        self.assertEqual(mock_remove.call_args.args[0], "PA69420")
+
+    @patch.object(Product, "reduce_count")
+    def test_product_reduce(self, mock_reduce: Mock):
+        ProductManager.reduce_count(ProductInfo(code="PA694200", count=100))
+        mock_reduce.assert_called_once()
+        self.assertEqual(mock_reduce.call_args.args[0], "PA694200")
+        self.assertEqual(mock_reduce.call_args.args[1], 100)
+
+    @patch("rich.print")
+    @patch("rich.table.Table.add_row")
+    def test_print_availability(self, mock_add_row: Mock, mock_print: Mock):
+        print_products = [
+            ProductInfo(
+                code="ProductAwesome",
+                description="I love this product",
+                brand="AwesomeBrand",
+                count_in_carton=10,
+                price=24000,
+                count=123654,
+            ),
+            ProductInfo(
+                code="ProductCoolest",
+                description="I cool this product",
+                brand="CoolBrand",
+                count_in_carton=12,
+                price=24001,
+                count=123664,
+            ),
+        ]
+
+        ProductManager.print_availability(print_products)
+
+        mock_print.assert_called_once()
+        mock_add_row.assert_any_call(
+            print_products[0].code,
+            print_products[0].description,
+            print_products[0].brand,
+            str(print_products[0].count_in_carton),
+            "IRR 24,000",
+            "IRR 2,967,696,000",
+            str(print_products[0].count),
+        )
+        mock_add_row.assert_any_call(
+            print_products[1].code,
+            print_products[1].description,
+            print_products[1].brand,
+            str(print_products[1].count_in_carton),
+            "IRR 24,001",
+            "IRR 2,968,059,664",
+            str(print_products[1].count),
+        )
+
+        mock_add_row.assert_any_call(
+            "Total", "", "", "", "", "IRR 5,935,755,664", "247318"
+        )
+
+    def test_save_availability(self):
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            filename = tmpfile.name + ".xlsx"
+            sample_availability = [
+                ProductInfo(
+                    code="P0012",
+                    description="Product 1",
+                    brand="BrandA",
+                    price=1000,
+                    count_in_carton=10,
+                    count=16,
+                ),
+                ProductInfo(
+                    code="P0028",
+                    description="Product 2",
+                    brand="BrandB",
+                    price=1234567890,
+                    count_in_carton=20,
+                    count=19,
+                ),
+                ProductInfo(
+                    code="P0030",
+                    description="Product 3",
+                    brand="BrandC",
+                    price=10000,
+                    count_in_carton=100,
+                    count=27,
+                ),
+            ]
+            
+            sample_availability_list = [
+                ["P0012", "Product 1", "BrandA", 10, 1000, 16000, 16],
+                ["P0028", "Product 2", "BrandB", 20, 1234567890, 23456789910, 19],
+                ["P0030", "Product 3", "BrandC", 100, 10000, 270000, 27],
+            ]
+            ProductManager.save_availability(filename, sample_availability)
+
+            reader = SheetReader(filename)
+            data = reader.get_data()
+
+            self.assertEqual(data[0], ["1"] + sample_availability_list[0])
+            self.assertEqual(data[1], ["2"] + sample_availability_list[1])
+            self.assertEqual(data[2], ["3"] + sample_availability_list[2])
 
 
 if __name__ == "__main__":
