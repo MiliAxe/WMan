bc1ceecd112738da9bd2c3c79a5268284f410869
Milad Zarei <miliaxe0@gmail.com>
2024-08-22 00:36:50 +0330

    chore: make the reader module able to start reading from a certain row

--- WMan/sheetutils/reader.py
+++ WMan/sheetutils/reader.py
@@ -4,11 +4,12 @@ from openpyxl import load_workbook
 class SheetReader:
     def __init__(self, filepath: str):
         self.workbook = load_workbook(filepath)
-        self.headers = []
         if self.workbook.active:
             self.sheet = self.workbook.active
 
-    def get_data(self):
-        read_data = [list(row) for row in self.sheet.iter_rows(values_only=True)]
-        self.headers = read_data[0]
-        return read_data[1:]
+    def get_data(self, start_row: int = 2):
+        read_data = [
+            list(row)
+            for row in self.sheet.iter_rows(values_only=True, min_row=start_row)
+        ]
+        return read_data
